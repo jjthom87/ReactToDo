ff2c56d64850e46ccaa94235ddbb79897162bc61
Jared Thomas <jjthom87@yahoo.com>
2016-09-16 00:10:31 -0400

    starting to do app

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 5000;
+
+app.use(function (req, res, next){
+	if (req.headers['x-forwarded-proto' === 'http']) {
+		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		next();
+	}
+})
+
+app.listen(PORT, function(){
+	console.log('Express Server is up on port ' + PORT);
+})
