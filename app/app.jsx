dde35a37a731909f34b7a2fd638bab777ff16b26
Jared Thomas <jjthom87@yahoo.com>
2016-09-16 00:19:26 -0400

    starting

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-
+var TodoApp = require('TodoApp')
 //load foundation
 $(document).foundation();
 
@@ -10,6 +10,6 @@ require('style!css!sass!applicationStyles')
 
 //calls all of the files here and renders to the html page
 ReactDOM.render(
-	<p>Boilerplate 3 Project</p>,
+	<TodoApp/>,
 	document.getElementById('app')
 );		
