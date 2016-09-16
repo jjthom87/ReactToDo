8568d5dbf513e220984554a9d82143ab5c527256
Jared Thomas <jjthom87@yahoo.com>
2016-09-16 00:44:03 -0400

    started todos

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,32 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+	getInitialState: function () {
+		return {
+			todos: [
+				{
+					id: 1,
+					text: 'get up early'
+				},{
+					id: 2,
+					text: 'code'
+				},{
+					id: 3, 
+					text: 'go to work'
+				},{
+					id: 4,
+					text: 'call pat bateman'
+				}
+			]
+		}
+	},
 	render: function () {
+		var {todos} = this.state;
+
 		return (
 			<div>
-				TodoApp.jsx
+				<TodoList todos={todos}/>
 			</div>
 		)
 	}
---
+++ app/components/TodoList.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+var Todo = require('Todo');
+
+var TodoList = React.createClass({
+	render: function () {
+		var {todos} = this.props;
+		//or this.props.todos
+		var renderTodos = () => {
+			return todos.map((todo) => {
+				return (
+					//key keeps track of your items
+					//...todo is a spread operator that passes it down as a prop
+					<Todo key={todo.id} {...todo}/>
+				);
+			});
+		};
+
+		return(
+			<div>
+				{renderTodos()}
+			</div>
+		)
+	}
+})
+
+module.exports = TodoList;
