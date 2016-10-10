ff14ae643f23bb417beea3fdcdb392d0d4d98c08
Jared Thomas <jjthom87@yahoo.com>
2016-10-09 23:36:03 -0400

    adding reducers actions and tests

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,9 @@ module.exports = {
 			'./app/api'
 		],
 		alias: {
-			applicationStyles: 'app/styles/app.scss'
+			applicationStyles: 'app/styles/app.scss',
+			actions: 'app/actions/actions.jsx',
+			reducers: 'app/reducers/reducers.jsx'
 		},
 		extensions: ['', '.js', '.jsx']
 	},
@@ -49,5 +51,5 @@ module.exports = {
 			path.resolve(__dirname, './node_modules/foundation-sites/scss')
 		]
 	},
-	devtool: 'cheap-module-eval-source-map'
+	devtool: 'source-map'
 };
