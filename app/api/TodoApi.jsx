b6bc29927e7cc76ef0f6eaf032b662501078208a
Jared Thomas <jaredthomas@Jareds-MacBook-Air.local>
2017-02-20 23:23:42 -0500

    heroku deploy

--- app/api/TodoApi.jsx
+++ app/api/TodoApi.jsx
@@ -1,35 +1,35 @@
-var $ = require('jQuery');
+// var $ = require('jQuery');
 
 module.exports = {
-	setTodos: function(todos){
-		if($.isArray(todos)){
-			localStorage.setItem('todos', JSON.stringify(todos));
-			return todos;
-		}
-	},
-	getTodos: function(){
-		var stringTodos = localStorage.getItem('todos');
-		var todos = [];
+	// setTodos: function(todos){
+	// 	if($.isArray(todos)){
+	// 		localStorage.setItem('todos', JSON.stringify(todos));
+	// 		return todos;
+	// 	}
+	// },
+	// getTodos: function(){
+	// 	var stringTodos = localStorage.getItem('todos');
+	// 	var todos = [];
 
-		try {
-			todos = JSON.parse(stringTodos);
-		} catch (err) {
+	// 	try {
+	// 		todos = JSON.parse(stringTodos);
+	// 	} catch (err) {
 
-		}
+	// 	}
 
-		//if true, code right after question mark gets executed
-		//if false, after colon
-		return $.isArray(todos) ? todos : []
+	// 	//if true, code right after question mark gets executed
+	// 	//if false, after colon
+	// 	return $.isArray(todos) ? todos : []
 
 
-		// if ($.isArray(todos)){
-		// 	return todos;
-		// } else {
-		// 	return [];
-		// }
+	// 	// if ($.isArray(todos)){
+	// 	// 	return todos;
+	// 	// } else {
+	// 	// 	return [];
+	// 	// }
 
-		//this is exactly the same as the ternary
-	},
+	// 	//this is exactly the same as the ternary
+	// },
 	filterTodos: function(todos, showCompleted, searchText){
 		var filteredTodos = todos;
 
